37240de820506484a4fcd2de1b315269957a2937
LookRain <brantscube@hotmail.com>
2017-06-14 22:38:12 +0800

    add option to select category when uploading

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,32 @@
       {{-- <upload-form></upload-form> --}}
       <form class="field" method="POST" action="/photos" enctype="multipart/form-data" name="file">
         {{ csrf_field() }}
+
+
         <input type="file" name="photo"></input>
-        <button class="button" type="submit">Save</button>
+
+        <label class="label">Category</label>
+        <p class="control">
+          <span class="select is-medium">
+            <select name="cat">
+              <option value="portrait">Portrait</option>
+              <option value="landscape">Landscape</option>
+              <option value="street">Street</option>
+
+            </select>
+          </span>
+        </p>
+
+        <button class="button is-primary" type="submit">Save</button>
+        @if (count($errors) > 0)
+        <div class="notification is-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+        @endif
       </form>
       @if (! empty($path))
       <img src="{{ Storage::url($path) }}" alt="">
