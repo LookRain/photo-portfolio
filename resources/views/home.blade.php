dd2080d4195a4772dffeee19ab281871c4072102
LookRain <brantscube@hotmail.com>
2017-06-14 22:37:59 +0800

    add more photo categories

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,27 @@
 <div class="is-flex" id="front" style="justify-content: center;"><h1 class="title">the gallery</h1></div>
 <section class="section">
 	<div class="container">
+		<div class="box">
+			<h1 class="title">Portrait</h1>
+			@foreach(Storage::files('public/images/portrait') as $link)
+			<img src="{{ Storage::url($link) }}" alt="" style=" max-height: 100%; max-width: 100%;">
+			@endforeach
+		</div>
 
-		@foreach(Storage::files('public/images') as $link)
+		<div class="box">
+			<h1 class="title">Landscape</h1>
+			@foreach(Storage::files('public/images/landscape') as $link)
+			<img src="{{ Storage::url($link) }}" alt="" style=" max-height: 100%; max-width: 100%;">
+			@endforeach
+		</div>
 
-		<img src="{{ Storage::url($link) }}" alt="" style=" max-height: 100%; max-width: 100%;">
-		@endforeach
+		<div class="box">
+			<h1 class="title">Street</h1>
+			@foreach(Storage::files('public/images/street') as $link)
+			<img src="{{ Storage::url($link) }}" alt="" style=" max-height: 100%; max-width: 100%;">
+			@endforeach
+		</div>
+		
 	</div>
 </section>
 @endsection
